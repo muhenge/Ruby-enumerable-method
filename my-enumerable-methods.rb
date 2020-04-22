e032052dedd3f4720705c37bd0fc80cbcf2592a4
muhenge <mugunga5@gmail.com>
2020-04-22 19:56:11 +0200

    add my_each_with_index method

--- my-enumerable-methods.rb
+++ my-enumerable-methods.rb
@@ -8,5 +8,22 @@ module Enumerable
 			to_enum(:my_each)
 		end
 	end
+	def my_each_with_index
+		if block_given?
+		   for counter in 0...size 
+			  yield self[counter] ,counter
+		   end
+		else
+		   to_enum(:my_each_with_index)
+		end
+	 end
 end
-[5,4,3].my_each do |x| p x end
+array = [5,4,3,2]
+puts "my_each"
+array.my_each do |x| p x end
+print "\n"
+puts "my_each_with_index"
+array.my_each_with_index do 
+	|elem,index| 
+	p "#{elem} => #{index}"
+end
