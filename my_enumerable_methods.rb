b8ad40454a59d33e30eb66feb976914370fb96da
Henzi <mugunga5@gmail.com>
2020-05-07 00:15:29 +0200

    correct my_each method

--- my_enumerable_methods.rb
+++ my_enumerable_methods.rb
@@ -1,17 +1,21 @@
 module Enumerable
   def my_each
+    counter = 0
+    arr = Array.new(counter)
     if block_given?
-      each do |counter|
-        yield(counter)
+      while counter < size
+        yield(arr[counter] = to_a[counter])
+        counter += 1
       end
+      arr
     else
-      to_enum(:my_each)
+      to_enum(:each)
     end
   end
 
   def my_each_with_index
     if block_given?
-      (0...size).each do |counter|
+      (0...size).my_each do |counter|
         yield self[counter], counter
       end
     else
@@ -20,34 +24,27 @@ module Enumerable
   end
 
   def my_select
-    select_in = []
-    my_each do |counter|
-      select_in << yield(counter)
+    if block_given?
+      select_in = Array.new
+      my_each { |counter|
+        select_in << counter if yield(counter)
+      }
+      select_in
+    else
+      to_enum(:select)
     end
-    select_in
   end
 
-  def my_all?
-    my_each do |element|
-      if block_given?
-        yield(element)
-      elsif element.nil? or !i or !yield(element)
-        puts false
-      end
+  def my_all?(args = nil)
+    bool = true
+    if args.nil?
+      block_given? ? my_each { |x| bool = false unless yield x } : bool = my_all? { |x| !x.nil? or (x != false) }
+    else
+      my_each { |x| bool = false unless args == x }
     end
-    puts true
+    bool
   end
 
-  def my_any?
-    my_each do |element|
-      if block_given?
-        yield element
-      elsif element.nil? or element or yield(element)
-        puts true
-      end
-    end
-    puts false
-  end
 
   def my_none?
     puts true if block_given?
@@ -86,10 +83,10 @@ module Enumerable
     return nil unless block_given?
 
     res = if !elmt.nil?
-            elmt
-          else
-            self[0]
-          end
+        elmt
+      else
+        self[0]
+      end
     self[0..-1].my_each { |i| res = yield(res, i) }
     res
   end
@@ -100,39 +97,37 @@ module Enumerable
 end
 
 array = [5, 4, 3, 2]
-puts 'my_each'
 array.my_each { |x| p x }
-print "\n"
-puts 'my_each_with_index'
+
 array.my_each_with_index do |elem, index|
   p "#{elem} => #{index}"
 end
-puts '--------------'
-puts 'my_select'
+puts "--------------"
+puts "my_select"
 
 array.my_select do |x|
   p x if x.even?
 end
-puts '--------------'
-puts 'my_all'
+puts "--------------"
+puts "my_all"
 
 array.my_all? { |num| num > 6 }
-puts '--------------'
-puts 'my_any'
+# puts '--------------'
+# puts 'my_any'
 
-array.my_any? { |num| num > 6 }
-puts '--------------'
-puts 'none'
+# array.my_any? { |num| num > 6 }
+# puts '--------------'
+# puts 'none'
 
-array.my_none? {}
-puts '--------------'
-puts 'my_count'
+# array.my_none? {}
+# puts '--------------'
+# puts 'my_count'
 
-puts array.my_count(&:even?)
-puts '--------------'
-puts 'my_map'
+# puts array.my_count(&:even?)
+# puts '--------------'
+# puts 'my_map'
 
-puts array.map(&:to_i)
-puts '--------------'
-puts 'my_inject'
-puts array.my_inject(3) { |num1, num2| num1 + num2 }
+# puts array.map(&:to_i)
+# puts '--------------'
+# puts 'my_inject'
+# puts array.my_inject(3) { |num1, num2| num1 + num2 }
---
+++ spec/enumerable_spec.rb
@@ -0,0 +1,252 @@
+require './my_enumerable_methods.rb'
+
+describe Enumerable do
+   describe '#my_each' do
+     context 'when a method have block' do
+       it 'return an array' do
+         expect([1, 2, 3, 4].my_each { |x| print x }).to eql([1, 2, 3, 4])
+       end
+     end
+     context 'a method has no block or no arguments' do
+       it 'return an enumerator if there is no block' do
+         expect([1, 2, 3, 4].my_each).to be_an(Enumerator)
+       end
+     end
+   end
+  end
+
+  # describe '#my_each_with_index' do
+  #   context 'when a method have block' do
+  #     it 'all the values has an index' do
+  #       hash = {}
+  #       [1, 2, 3, 4].my_each_with_index { |val, index| hash[val] = index }
+  #       expect(hash).to eql({ 1 => 0, 2 => 1, 3 => 2, 4 => 3 })
+  #     end
+  #   end
+
+  #   context 'a method has no block or no arguments' do
+  #     it 'return an enumerator if there is no block' do
+  #       expect([1, 2, 3, 4].my_each_with_index).to be_an(Enumerator)
+  #     end
+  #   end
+  # end
+
+  # describe '#my_select' do
+  #   context 'when a method have block' do
+  #     it 'selected values are multiple of three' do
+  #       expect([1, 2, 3, 4, 5, 6, 7, 8, 9].my_select { |x| (x % 3).zero? }).to eql([3, 6, 9])
+  #     end
+
+  #     it 'selected values are multiple of two' do
+  #       expect([1, 2, 3, 4, 5, 6, 7, 8, 9].my_select { |x| (x % 2).zero? }).to eql([2, 4, 6, 8])
+  #     end
+  #   end
+
+  #   context 'a method has no block or no arguments' do
+  #     it 'return an enumerator if there is no block' do
+  #       expect([1, 2, 3, 4, 5, 6, 7, 8, 9].my_select).to be_an(Enumerator)
+  #     end
+  #   end
+  # end
+
+  # describe '#my_all?' do
+  #   context 'when a method have block' do
+  #     it 'all the values are greater or equal than 3' do
+  #       expect([8, 4, 3, 9, 5].my_all? { |x| x >= 3 }).to eql(true)
+  #     end
+
+  #     it 'all the values are not greater or equal than 4' do
+  #       expect([8, 4, 3, 9, 5].my_all? { |x| x >= 4 }).to eql(false)
+  #     end
+  #   end
+
+  #   context 'when a method has a argument' do
+  #     it 'all the values are not equal than 5' do
+  #       expect([8, 4, 3, 9, 5].my_all?(5)).to eql(false)
+  #     end
+
+  #     it 'all the values are equal than 1' do
+  #       expect([1, 1, 1, 1, 1].my_all?(1)).to eql(true)
+  #     end
+
+  #     it 'all the values are numbers' do
+  #       expect([8, 4, 3, 9, 5].my_all?(Numeric)).to eql(true)
+  #     end
+
+  #     it 'all the values are not Strings' do
+  #       expect([8, 4, 3, 9, 5].my_all?(String)).to eql(false)
+  #     end
+
+  #     it 'all the values have a letter \'a\' inside' do
+  #       expect(%w[car bar chair].my_all?(/a/)).to eql(true)
+  #     end
+
+  #     it 'all the values do not have a letter \'h\' inside' do
+  #       expect(%w[car bar chair].my_all?(/h/)).to eql(false)
+  #     end
+  #   end
+
+  #   context 'a method has no block or no arguments' do
+  #     it 'none of the items are nil or false' do
+  #       expect([45, 67, 20, 9].my_all?).to eql(true)
+  #     end
+
+  #     it 'one of the items is false or nil' do
+  #       expect([nil].my_all?).to eql(false)
+  #     end
+  #   end
+  # end
+
+  # describe '#my_any?' do
+  #   context 'a method has a block' do
+  #     it 'at least one of the values is greater or equal than 3' do
+  #       expect([8, 4, 3, 9, 5].my_any? { |x| x >= 3 }).to eql(true)
+  #     end
+
+  #     it "there isn't any value greater or equal than 10" do
+  #       expect([8, 4, 3, 9, 5].my_any? { |x| x >= 10 }).to eql(false)
+  #     end
+  #   end
+  #   context 'a method has arguments' do
+  #     it 'at least one of the values is equal than 8' do
+  #       expect([8, 'd', 'e', 9, 5].my_any?(8)).to eql(true)
+  #     end
+
+  #     it "there isn't any value equal than 1" do
+  #       expect([3, 4, 5, 6, 7].my_any?(1)).to eql(false)
+  #     end
+
+  #     it 'at least one of the values has a letter \'h\' inside' do
+  #       expect(%w[car bar chair].my_any?(/h/)).to eql(true)
+  #     end
+
+  #     it 'at least one of the values do not have a letter \'y\' inside' do
+  #       expect(%w[car bar chair].my_any?(/y/)).to eql(false)
+  #     end
+
+  #     it 'at least one of the items is Integer' do
+  #       expect([45, 67, 20, 'jias'].my_any?(Integer)).to eql(true)
+  #     end
+
+  #     it 'any of the items is String' do
+  #       expect([nil].my_any?(String)).to eql(false)
+  #     end
+  #   end
+
+  #   context 'a method has no block or no arguments' do
+  #     it 'at least one of the items is not nil or false' do
+  #       expect([45, 67, 20, nil].my_any?).to eql(true)
+  #     end
+
+  #     it 'all the items are nil' do
+  #       expect([nil].my_any?).to eql(false)
+  #     end
+  #   end
+  # end
+
+  # describe '#my_none?' do
+  #   context 'a method has a block' do
+  #     it 'the numbers are not negative' do
+  #       expect([20, 3, 6, 10].my_none? { |x| x < 0 }).to eql(true)
+  #     end
+
+  #     it 'the numbers are not positive' do
+  #       expect([20, 3, 6, 10].my_none? { |x| x > 0 }).to eql(false)
+  #     end
+  #   end
+
+  #   context 'a method has arguments' do
+  #     it 'there is not a number 7 inside the array' do
+  #       expect([20, 3, 6, 10].my_none?(7)).to eql(true)
+  #     end
+
+  #     it 'there is a number 6 inside the array' do
+  #       expect([20, 3, 6, 10].my_none?(6)).to eql(false)
+  #     end
+
+  #     it 'none of the values has a letter \'y\' inside' do
+  #       expect(%w[car bar chair].my_none?(/y/)).to eql(true)
+  #     end
+
+  #     it 'at least one of the values have a letter \'h\' inside' do
+  #       expect(%w[car bar chair].my_none?(/h/)).to eql(false)
+  #     end
+  #     it 'none of the items is a string' do
+  #       expect([45, 67, 20, 9].my_none?(String)).to eql(true)
+  #     end
+
+  #     it 'one of the items is integer' do
+  #       expect([4].my_none?(Integer)).to eql(false)
+  #     end
+  #   end
+
+  #   context 'a method has no arguments or a block' do
+  #     it 'none of the items is true' do
+  #       expect([45, 67, 20, 9].my_none?).to eql(true)
+  #     end
+
+  #     it 'one of the items is true' do
+  #       expect([true].my_none?).to eql(false)
+  #     end
+  #   end
+  # end
+
+  # describe '#my_count' do
+  #   context 'a method has a block' do
+  #     it 'all numbers that are equal to 3 are counted' do
+  #       expect([20, 3, 6, 10].my_count { |x| x == 3 }).to eql(1)
+  #     end
+  #   end
+
+  #   context 'a method has an argument' do
+  #     it 'all numbers that are equal to 5 are counted' do
+  #       expect([20, 3, 6, 10].my_count(5)).to eql(0)
+  #     end
+  #   end
+
+  #   context 'a method has no argument or a block' do
+  #     it 'all element are in array are counted' do
+  #       expect([20, 3, 6, 10].my_count).to eql(4)
+  #     end
+  #   end
+  # end
+
+  # describe '#my_map' do
+  #   context 'a method has a block' do
+  #     it 'all the number are multiplied by 2' do
+  #       expect([20, 3, 6, 10].my_map { |x| x * 2 }).to eql([40, 6, 12, 20])
+  #     end
+  #   end
+  #   context 'a method no block and parameters' do
+  #     it 'return an enumerator if there is no block' do
+  #       expect([20, 3, 6, 10].my_map).to be_an(Enumerator)
+  #     end
+  #   end
+  # end
+
+  # describe '#my_inject' do
+  #   context 'a method has a block' do
+  #     it 'the result of the sum of numbers inside the array is 39' do
+  #       expect([20, 3, 6, 10].my_inject { |x, y| x + y }).to eql(39)
+  #     end
+  #   end
+
+  #   context 'a method has a symbol as a parameter' do
+  #     it 'the result of the sum of numbers inside the array is 39' do
+  #       expect([20, 3, 6, 10].my_inject(:+)).to eql(39)
+  #     end
+  #   end
+
+  #   context 'a method has a number and a symbol as a parameter' do
+  #     it 'the result of the multiplication by 3 of the numbers inside the array is 120' do
+  #       expect([1, 2, 4, 5].my_inject(3, :*)).to eql(120)
+  #     end
+  #   end
+
+  #   context 'a method has a number as a parameter and a block' do
+  #     it 'the result of the addition of the numbers inside the array and the parameter 3 is 15' do
+  #       expect([1, 2, 4, 5].my_inject(3) { |x, y| x + y }).to eql(15)
+  #     end
+  #   end
+  # end
+#end
